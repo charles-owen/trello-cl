17df8e63f25e598ae0f445354993fda6826d4ca1
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:07:19 -0500

    New cl-publish system.

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,7 @@
 const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
 	entry: {
 		Trello: path.resolve(__dirname, 'index.js')
-	},
-	plugins: [
-		new FileManagerPlugin({
-			onEnd: {
-				copy: [
-					{source: path.resolve(__dirname, '../../../cl/dist/trello.*'), destination: path.resolve(__dirname, 'dist') }
-				]
-			}
-		})
-	]
+	}
 }
